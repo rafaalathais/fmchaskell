0ecf371045bd09deee0ab2d9b5e14db957425b0b
Rafaela Thais <rafaelathaisabreu28@gmail.com>
2025-09-29 23:51:01 -0300

    show, toEnum, fromEnum, AND, OR, NAND e XOR resolvidos

--- Solutions/FMCBool.hs
+++ Solutions/FMCBool.hs
@@ -16,7 +16,9 @@ import Prelude
     , undefined
     , error
     , otherwise
+    , String
     )
+import System.Win32 (xBUTTON1)
 
 -- Define evenerything that is undefined,
 -- without using standard Haskell functions.
@@ -26,29 +28,40 @@ data Bool = False | True
 
 instance Show Bool where
 
-    show = undefined
+    show :: Bool -> String
+    show True = "tt"
+    show False = "ff"
 
 instance Enum Bool where
 
-    toEnum  = undefined
+    toEnum :: Int -> Bool
+    toEnum 0 = False
+    toEnum 1 = True
 
-    fromEnum  = undefined
+    fromEnum :: Bool -> Int
+    fromEnum True = 1
+    fromEnum False = 0
 
 -- conjunction (AND)
 (&&) :: Bool -> Bool -> Bool
-(&&) = undefined
+True && True = True
+_ && _ = False
 
 infixr 3 &&
 
 -- disjunction (OR)
 (||) :: Bool -> Bool -> Bool
-(||) = undefined
+True || x = True
+False || False = False
+x || True = True
 
 infixr 2 ||
 
 -- NAND (Sheffer stroke)
 (/|\) :: Bool -> Bool -> Bool
-(/|\) = undefined
+False /|\ x = True
+True /|\ True = False
+True /|\ False = True
 
 infixr 2 /|\
 
@@ -60,7 +73,13 @@ infixr 2 \|/
 
 -- XOR (exclusive disjunction)
 (<=/=>) :: Bool -> Bool -> Bool
-(<=/=>) = undefined
+x <=/=> y = case fromEnum x of
+                1 -> case fromEnum y of
+                    1 -> False
+                    0 -> True
+                0 -> case fromEnum y of
+                    0 -> False
+                    1 -> True
 
 infixr 2 <=/=>
 
